174a1178e162b2fbee22d5944a5c63ebb7595721
Nikita <nrj.tomsk@gmail.com>
2017-12-06 08:51:42 +0700

    test

--- src/http/Controller/HomeController.php
+++ src/http/Controller/HomeController.php
@@ -23,8 +23,6 @@ class HomeController extends Controller
      */
     public function anyIndex()
     {
-        $refer = $this->request->get('refer');
-
         return ['thunder' => ['say' => 'Hello world'], 'thunder_ver' => '0.0.1-alpha', 'refer' => $refer];
     }
 }
