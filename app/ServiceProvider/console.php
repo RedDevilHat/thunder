673e09eff0c054a731a2b3c4da6e626ad8dd043d
Nikita <nrj.tomsk@gmail.com>
2017-12-12 09:52:53 +0700

    Add Phinx

--- app/ServiceProvider/console.php
+++ app/ServiceProvider/console.php
@@ -12,6 +12,14 @@ $kernel->call(function () use ($kernel) {
 
     $console->add(new \Symfony\Component\Console\Command\ListCommand());
     $console->add(new \Src\Console\HelloWorldCommand());
-    $console->add(new \etc\console\MigrationMakeCommand());
+    $console->add(new Phinx\Console\Command\Status());
+    $console->add(new Phinx\Console\Command\Init());
+    $console->add(new Phinx\Console\Command\Create());
+    $console->add(new Phinx\Console\Command\Migrate());
+    $console->add(new Phinx\Console\Command\Breakpoint());
+    $console->add(new Phinx\Console\Command\Rollback());
+    $console->add(new Phinx\Console\Command\SeedCreate());
+    $console->add(new Phinx\Console\Command\SeedRun());
+    $console->add(new Phinx\Console\Command\Test());
     $console->run();
 });
